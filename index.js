f419628c531f2118de2942b2765f7d105899c82a
gcamposl <gcamposlopes@gmail.com>
2024-04-10 23:26:29 -0300

    sma implement

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const axios = require("axios");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 68500;
-const SELL_PRICE = 69300;
+const BUY_PRICE = 70300;
+const SELL_PRICE = 70500;
 let isOpened = false;
 
 const API_URL_HML = "https://testnet.binance.vision";
 //https://api.binance.com
 
-async function start() {
-  const { data } = await axios.get(
-    API_URL_HML + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL
-  );
-  const candle = data[data.length - 1];
-  const price = parseFloat(candle[4]);
-  console.clear();
-  console.log("Price: " + price);
+function smaCalculate(data){
+	const close = data.map(candle => parseFloat(candle[4]));
+	const sum = close.reduce((a,b) => a + b);
+	return sum / data.length;
+}
 
-  if (price <= BUY_PRICE && isOpened === false) {
-    console.log("buy");
-    isOpened = true;
-  } else if (price >= SELL_PRICE && isOpened === true) {
-    console.log("sell");
-    isOpened = false;
-  }
-  else
-    console.log("await");
+async function start() {
+	const { data } = await axios.get(
+		API_URL_HML + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL
+  	);
+  	const candle = data[data.length - 1];
+  	const price = parseFloat(candle[4]);
+  	console.clear();
+  	console.log("Price: " + price);
+
+	const sma21 = smaCalculate(data);
+	const sma13 = smaCalculate(data.slice(8));
+	console.log("SMA (13): " + sma13);
+	console.log("SMA (21): " + sma21);
+	console.log("Is Opened? " + isOpened)
+
+	if (sma13 > sma21 && isOpened === false) {
+		console.log("buy");
+		isOpened = true;
+	} else if (sma21 < sma13 && isOpened === true) {
+		console.log("sell");
+		isOpened = false;
+	} else
+		console.log("await");
 }
 
 setInterval(start, 3000);
